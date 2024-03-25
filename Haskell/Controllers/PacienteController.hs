5841e7c04208befb1d7261b5ff39918cb8a46742
gabrielavmm <gabriela.virginia.melo.mendes@ccc.ufcg.edu.br>
2024-03-25 12:28:56 -0300

    feat: Novos filtros

--- Haskell/Controllers/PacienteController.hs
+++ Haskell/Controllers/PacienteController.hs
@@ -32,35 +32,44 @@ Cria um paciente.
 criaPaciente :: Int -> [String] -> Paciente
 criaPaciente idP infos = read (intercalate ";" ([show (idP)] ++ infos)) :: Paciente
 
-{-
-Essa função filtra uma lista de médicos com base na especialidade desejada.
+
+{-Essa função filtra uma lista de médicos com base na especialidade desejada.
 @param especialidadeDesejada: A especialidade que se deseja encontrar nos médicos.
 @param medicos: Uma lista de médicos que será filtrada.
 @return Uma lista de médicos que possuem a especialidade desejada.
 -}
-
 filtrarMedicosPorEspecialidade :: String -> [Medico.Medico] -> [Medico.Medico]
 filtrarMedicosPorEspecialidade especialidadeDesejada medicos  =
     filter (\medico -> Medico.especialidade medico == especialidadeDesejada) medicos
 
 {-Essa função filtra a clinica especifica desejeda.
 @param nomeEspecifico: a clinica desejada
-@param clinicas: a lista das clinias qu serão filtradas
+@param clinicas: a lista das clinias que serão filtradas
 @return a clinica desjada
 -}
-
 filtrarPorClinica :: String -> [Clinica.Clinica] -> [Clinica.Clinica]
 filtrarPorClinica nomeEspecifico clinicas = 
     filter (\clinica -> Clinica.nome clinica == nomeEspecifico) clinicas
+
+
+{-Essa função filtra um médico.
+@param medicoEspecifico: o médico desejado.
+@param medicos: a lista de médico que será filtrada
+-}
+filtrarPorMedico :: String ->[Medico.Medico] -> [Médico.Médico]
+filtrarPorMedico medicoEspecifico medicos =
+    filter(\medico -> Medico.nome medico == medicoEspecifico ) medicos
+
+
 {-Essa função filtra uma lista de clínicas com base no plano de saúde desejado.
    @param planoSaudeDesejado: O plano de saúde desejado.
    @param clinicas: A lista de clínicas que será filtrada.
    @return A lista de clínicas que aceitam o plano de saúde desejado.
 -}
-
 filtrarClinicasPorPlanoDeSaude :: String -> [Clinica.Clinica] -> [Clinica.Clinica]
 filtrarClinicasPorPlanoDeSaude planoSaudeDesejado clinicas =
     filter (\clinica -> elem planoSaudeDesejado (Clinica.planos clinica)) clinicas
+
 
 {-Essa função filtra uma lista de clínicas com base no tipo de agendamento desejado.
    @param tipoAgendamentoDesejado: O tipo de agendamento desejado (por exemplo, "hora marcada" ou "ordem de chegada").
@@ -72,20 +81,25 @@ filtrarClinicasPorAgendamento tipoAgendamentoDesejado clinicas =
     filter (\clinica -> Clinica.tipoAgendamento clinica == tipoAgendamentoDesejado) clinicas
 
 
-{-Essa função filtra uma lista de médicos ou clínicas com base em uma lista de filtros.
-   Cada filtro é um par contendo o tipo de filtro e o valor desejado.
-   Os tipos de filtro podem ser "especialidade", "clinica", "plano" ou "agendamento".
-   @param filtros: Uma lista de pares contendo o tipo de filtro e o valor desejado.
-   @param opcoes: Uma lista de médicos ou clínicas que será filtrada.
-   @return Uma lista de médicos ou clínicas que atendem aos critérios de todos os filtros aplicados.
+{-Esta função filtra uma lista de médicos de acordo com sintomas especificos
+@return uma lista de médicos filtrada de acordo com o sintoma.
 -}
-filtrarOpcoes :: [(String, String)] -> [a] -> [a]
-filtrarOpcoes filtros opcoes = foldl (\opts (filtro, valor) -> case filtro of
-        "especialidade" -> filtrarMedicosPorEspecialidade valor opts
-        "clinica" -> filtrarPorClinica valor opts
-        "plano" -> filtrarClinicasPorPlanoDeSaude valor opts
-        "agendamento" -> filtrarClinicasPorAgendamento valor opts
-        _ -> opts) opcoes filtros
+filtrarMedicoPorSintoma :: String -> [Medico.Medico] -> [Medico.Medico]
+filtrarMedicoPorSintoma sintoma medicos =
+    | sintoma == "dor nas costas" || sintoma == "dor lombar" ||"dor na mão" || "dor no pé" || "dor na ombro " = filtrarMedicosPorEspecialidade "Ortopedista" medicos
+    | sintoma == "enxaqueca" || sintoma == "dor de cabeça" = filtrarMedicosPorEspecialidade "Neurologia" medicos
+    | sintoma == "dor de garganta" || sintoma == "nariz entupido" || sintoma == "sinusite" || sintoma == "dor de ouvido" = filtrarMedicosPorEspecialidade "Otorrinolaingologista" medicos
+    | sintoma == "menstruação desrregulada" || sintoma == "gravidez" || sintoma == "cólica" = filtrarMedicosPorEspecialidade "Ginecologista" medicos
+    | sintoma == "espinhas" || sintoma == "queda de cabelo" || sintoma == "mancha na pele" = filtrarMedicosPorEspecialidade "Dermatologista" medicos
+    | sintoma == "desconforto abdominal" || sintoma == "azia" || sintoma == "gastrite" = filtrarMedicosPorEspecialidade "Gastroenterologia" medicos
+    | sintoma == "febre" || sintoma == "tosse" || sintoma == "resfriado" || sintoma == "dor de garganta" = filtrarMedicosPorEspecialidade "Clínico Geral" medicos
+    | sintoma == "pressão alta" || sintoma == "pressão baixa" = filtrarMedicosPorEspecialidade "Cardiologia" medicos
+    | sintoma == "ansiedade" || sintoma == "depressão" = filtrarMedicosPorEspecialidade "Psiquiatria" medicos
+    | sintoma == "visão embaçada" || sintoma == "olho vermelho" || sintoma == "coceira nos olhos" = filtrarMedicosPorEspecialidade "Oftalmologia" medicos
+    | sintoma == "queda de pressão" || sintoma == "desmaio" = filtrarMedicosPorEspecialidade "Cardiologia" medicos
+    | sintoma == "dor nos rins" || sintoma == "sangue na urina" || sintoma == "dificuldade para urinar" = filtrarMedicosPorEspecialidade "Urologia" medicos
+    | otherwise = putStrLn "Não foi possível achar médicos para este sintoma"
+
 
 {-
 Essa função filtra uma lista de laudos com base no id do paciente.
@@ -96,6 +110,7 @@ Essa função filtra uma lista de laudos com base no id do paciente.
 consultarLaudo :: Int -> [Laudo] -> [Laudo]
 consultarLaudo _ [] = []
 consultarLaudo idPaciente laudos = filter (\laudo -> Laudo.id laudo == idPaciente) laudos
+
 
 {-
 Essa função filtra uma lista de receitas com base no id do paciente.
