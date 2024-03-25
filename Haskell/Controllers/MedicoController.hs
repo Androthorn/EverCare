d7e68809d21c45d3b80dca540e7609b28fe24943
Guilherme Boia <guilherme.dantas.boia.albuquerque@ccc.ufcg.edu.br>
2024-03-25 14:35:50 -0300

    bugfix: remove chat controller

--- Haskell/Controllers/MedicoController.hs
+++ Haskell/Controllers/MedicoController.hs
@@ -30,52 +30,6 @@ consultarReceita :: Int -> [Receita.Receita] -> [Receita.Receita]
 consultarReceita _ [] = []
 consultarReceita idPaciente receitas = filter (\receita -> Receita.idPaciente receita == idPaciente) receitas
 
--- Função para enviar mensagem de médico para paciente
-enviarMensagemParaPaciente :: Int -> String -> IO ()
-enviarMensagemParaPaciente idPaciente mensagem = do
-    appendFile "mensagens.txt" $ "M:" ++ show idPaciente ++ ";" ++ mensagem ++ "\n"
-
--- Função para listar todas as mensagens recebidas por um médico
-listarMensagensRecebidas :: Int -> IO ()
-listarMensagensRecebidas idMedico = do
-    conteudo <- readFile "mensagens.txt"
-    let mensagens = filter (\m -> take 2 m == "P:" ++ show idMedico) (lines conteudo)
-    mapM_ (mostrarMensagem) mensagens
-
--- Função para ler mensagens de um paciente específico
-lerMensagensDePaciente :: Int -> Int -> IO ()
-lerMensagensDePaciente idMedico idPaciente = do
-    conteudo <- readFile "mensagens.txt"
-    let mensagens = filter (\m -> take 4 m == "M:" ++ show idMedico ++ ";" ++ show idPaciente) (lines conteudo)
-    mapM_ (mostrarMensagem) mensagens
-
--- Função para mostrar a mensagem com o nome do remetente no início
-mostrarMensagem :: String -> IO ()
-mostrarMensagem mensagem = do
-    let (tipo, idRemetente, texto) = splitMessage mensagem
-    case tipo of
-        "P" -> do
-            paciente <- getNomePaciente (read idRemetente)
-            putStrLn $ "Paciente " ++ nome paciente ++ ": " ++ texto
-        _ -> putStrLn $ "Médico: " ++ texto
-
--- Função para obter o nome do paciente pelo ID
-getNomePaciente :: Int -> IO Paciente
-getNomePaciente idPaciente = do
-    conteudo <- readFile "pacientes.txt"
-    let pacientes = lines conteudo
-    let pacienteInfo = filter (\p -> take 1 p == show idPaciente) pacientes
-    return $ parsePaciente (head pacienteInfo)
-
--- Função para dividir uma mensagem nos campos tipo, ID do remetente e texto
-splitMessage :: String -> (String, String, String)
-splitMessage mensagem = (tipo, idRemetente, texto)
-    where
-        (tipo:idRemetente:texto) = split ';' mensagem
-
--- Função para analisar uma string e criar um objeto Paciente
-parsePaciente :: String -> Paciente
-parsePaciente str = undefined  -- Implemente conforme a estrutura do seu modelo Paciente
 
 {- 
 Essa função retorna o ID do medico dado o seu nome.
--- Haskell/Controllers/PacienteController.hs
+++ Haskell/Controllers/PacienteController.hs
@@ -137,52 +137,6 @@ consultarReceita :: Int -> [Receita.Receita] -> [Receita.Receita]
 consultarReceita _ [] = []
 consultarReceita idPaciente receita = filter (\receita -> Receita.idPaciente receita == idPaciente) receita
 
--- Função para enviar mensagem de paciente para médico
-enviarMensagemParaMedico :: Int -> String -> IO ()
-enviarMensagemParaMedico idMedico mensagem = do
-    appendFile "mensagens.txt" $ "P:" ++ show idMedico ++ ";" ++ mensagem ++ "\n"
-
--- Função para listar todas as mensagens recebidas por um paciente
-listarMensagensRecebidas :: Int -> IO ()
-listarMensagensRecebidas idPaciente = do
-    conteudo <- readFile "mensagens.txt"
-    let mensagens = filter (\m -> take 2 m == "M:" ++ show idPaciente) (lines conteudo)
-    mapM_ (mostrarMensagem) mensagens
-
--- Função para ler mensagens de um médico específico
-lerMensagensDeMedico :: Int -> Int -> IO ()
-lerMensagensDeMedico idPaciente idMedico = do
-    conteudo <- readFile "mensagens.txt"
-    let mensagens = filter (\m -> take 4 m == "P:" ++ show idPaciente ++ ";" ++ show idMedico) (lines conteudo)
-    mapM_ (mostrarMensagem) mensagens
-
--- Função para mostrar a mensagem com o nome do remetente no início
-mostrarMensagem :: String -> IO ()
-mostrarMensagem mensagem = do
-    let (tipo, idRemetente, texto) = splitMessage mensagem
-    case tipo of
-        "M" -> do
-            medico <- getNomeMedico (read idRemetente)
-            putStrLn $ "Médico " ++ nome medico ++ ": " ++ texto
-        _ -> putStrLn $ "Paciente: " ++ texto
-
--- Função para obter o nome do médico pelo ID
-getNomeMedico :: Int -> IO Medico
-getNomeMedico idMedico = do
-    conteudo <- readFile "medicos.txt"
-    let medicos = lines conteudo
-    let medicoInfo = filter (\m -> take 1 m == show idMedico) medicos
-    return $ parseMedico (head medicoInfo)
-
--- Função para dividir uma mensagem nos campos tipo, ID do remetente e texto
-splitMessage :: String -> (String, String, String)
-splitMessage mensagem = (tipo, idRemetente, texto)
-    where
-        (tipo:idRemetente:texto) = split ';' mensagem
-
--- Função para analisar uma string e criar um objeto Médico
-parseMedico :: String -> Medico
-parseMedico str = undefined  -- Implemente conforme a estrutura do seu modelo Médico
 
 
 {- 
