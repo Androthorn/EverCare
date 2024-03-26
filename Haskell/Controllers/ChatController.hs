687b92c9a5d789e8cc3650112e3def200e4847f5
ana <ana.ceci7373@gmail.com>
2024-03-26 20:31:22 -0300

    de

--- Haskell/Controllers/ChatController.hs
+++ Haskell/Controllers/ChatController.hs
@@ -15,21 +15,15 @@ import qualified Haskell.Models.Medico as Medico
 criaChat :: Int -> Int -> String -> IO ()
 criaChatP idRemetente idDestinatario mensagem = read (intercalate ";" ([show (idPaciente), show (idMedico), mensagem])) :: Chat
    
--- Função para enviar mensagem de paciente para médico
+-- Função para enviar mensagem 
 enviarMensagem :: Int -> String -> IO ()
 enviarMensagem idChat mensagem = read (intercalate ";" ([show (idChat), mensagem])) :: Chat
 
-
-enviarMensagemParaPaciente :: Int -> String -> IO ()
-enviarMensagemParaPaciente idChat mensagem = read (intercalate ";" ([show (idChat), mensagem])) :: Chat
-
--- Função para listar todas as mensagens recebidas por um paciente
+-- Função para listar todas as mensagens recebidas 
 listarChats :: Int -> [Chat.Chat] -> IO ()
 listarChats idID chats = filter (\chat -> Chat.idPaciente chat == idPaciente) chats
 
-
-
 -- Função para ler mensagens de um médico específico
-lerMensagemPessoa :: Int -> [Chat.Chat] -> IO ()
-lerMensagensDeMedico idChat chats = filter (\chat -> Chat.id chat == idChat) chats
+lerChatEspecifico:: Int -> [Chat.Chat] -> IO ()
+lerChatEspecifico idChat chats = filter (\chat -> Chat.id chat == idChat) chats
 
