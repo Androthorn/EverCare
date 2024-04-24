1651d315fc1263d5aaf73d71ae54d969fa5a4585
Guilherme Boia <guilherme.dantas.boia.albuquerque@ccc.ufcg.edu.br>
2024-04-24 20:58:46 -0300

    build: inicia estrutura do projeto em prolog

---
+++ Prolog/App/utils.pl
@@ -0,0 +1,25 @@
+:- module(utils, [prompt/2, promptString/2, autentica/3, mensagemEspera/0, promptOption/2, promptPassword/2]).
+
+:- use_module('../Models/model.pro').
+
+prompt(Text, Value) :- promptString(Text, V), atom_number(V, Value).
+
+
+promptString(Text, Value) :- read_pending_chars(user_input, _, _),
+                             write(Text), flush_output(user),
+                             read_line_to_string(user_input, Value).
+
+
+promptOption(Text, Value) :- promptString(Text, V), string_upper(V, Value).
+
+
+promptPassword(Text, Value) :- promptString(Text, V),
+    (V = "" -> promptPassword(Text, Value) ;
+    Value = V).
+
+
+autentica(ID, Senha, Tipo) :- model:logins(ID, Senha, Tipo), !.
+autentica(_, _, Tipo) :- Tipo is -1.
+
+
+mensagemEspera :- promptString('\n\nPressione qualquer tecla para continuar', _), tty_clear.
