2987a30c76dfd7228799c3abe5ee621e2b0f2e6c
anavirginianery <ana.virginia.souza.nery@ccc.ufcg.edu.br>
2024-04-30 20:43:00 -0300

    cleaning code

--- Prolog/Controllers/pacienteController.pl
+++ Prolog/Controllers/pacienteController.pl
@@ -28,15 +28,15 @@
 :- use_module('../App/show.pl').
 
 
-/* 
-Cria uma consulta.
+# /* 
+# Cria uma consulta.
 
-@param ID: id da consulta.
-@param IDClinica: id da clínica.
-@param IDMedico: id do médico.
-@param Data: data da consulta.
-@param Horario: horário da consulta.
-@param Queixa: queixa do paciente.
-*/
-cadastraConsulta(IDClinica, IDMedico, Data, Horario, Queixa) :- 
-    assertz(model:consulta(ID, IDClinica, IDMedico, Data, Horario, Queixa)).
+# @param ID: id da consulta.
+# @param IDClinica: id da clínica.
+# @param IDMedico: id do médico.
+# @param Data: data da consulta.
+# @param Horario: horário da consulta.
+# @param Queixa: queixa do paciente.
+# */
+# cadastraConsulta(IDClinica, IDMedico, Data, Horario, Queixa) :- 
+#     assertz(model:consulta(ID, IDClinica, IDMedico, Data, Horario, Queixa)).
