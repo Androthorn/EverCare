2210e5ec59540eccfc482d59805eafd950b9ff87
ana <ana.ceci7373@gmail.com>
2024-05-02 15:48:03 -0300

    alterando o controller clinica

--- Prolog/Controllers/clinicaController.pl
+++ Prolog/Controllers/clinicaController.pl
@@ -1,4 +1,4 @@
-:- module(clinica, [verAgendamentoClin/1, verMedicos/1, verPaciente/4]).
+:- module(clinica, [verAgendamentoClin/1, verMedicos/1, verPaciente/1]).
 
 :- use_module('../App/show.pl').
 :- use_module('../Models/model.pl').
@@ -7,27 +7,22 @@
 verAgendamentoClin(IdClinica) :- forall(model:consulta(IdCons, IdClinica, IdMedico, IDPac, DataConsulta, HoraConsulta, Queixas, C), 
             show:showConsulta(model:consulta(IdCons, IdClinica, IdMedico, IDPac, DataConsulta, HoraConsulta, Queixas, C))).  
 
-verPaciente(_, [], [], "Não há pacientes nessa clínica").
-verPaciente(_, [], _, "").
-verPaciente(_, _, [], "").
-
-verPaciente(IdClinica, Consultas, Pacientes, InfoPacientes) :-
-    findall(IDPac, (member(consulta(_, IdClinica, _, IDPac, _, _, _, _), Consultas)), PacientesConsulta),
+verPaciente(IdClinica) :-
+    findall(IDPac, model:consulta(_, IdClinica, _, IDPac, _, _, _, _), PacientesConsulta),
     list_to_set(PacientesConsulta, PacientesFiltrados),
-    getPacientes(PacientesFiltrados, Pacientes, PacientesOK),
-    showLista(PacientesOK, InfoPacientes).
+    getPacientes(PacientesFiltrados, Pacientes),
+    showPacientes(Pacientes).
 
-getPacientes([], _, []).
-getPacientes([IDPac|T], Pacientes, [Paciente|PacientesOK]) :-
-    member(Paciente, Pacientes),
-    paciente(IDPac, Paciente),
-    getPacientes(T, Pacientes, PacientesOK).
+getPacientes([], []).
+getPacientes([IDPac|T], [Paciente|Pacientes]) :-
+    model:paciente(IDPac, Nome, Cpf, DataNascimento, Sexo, Endereco, TipoSanguineo, Cardiopata, Hipertenso, Diabetico, _, _),
+    Paciente = paciente(IDPac, Nome, Cpf, DataNascimento, Sexo, Endereco, TipoSanguineo, Cardiopata, Hipertenso, Diabetico),
+    getPacientes(T, Pacientes).
 
-showLista([], "").
-showLista([Paciente|T], InfoPacientes) :-
-    showPaciente(Paciente, InfoPaciente),
-    showLista(T, Resto),
-    string_concat(InfoPaciente, Resto, InfoPacientes).
+showPacientes([]).
+showPacientes([Paciente|T]) :-
+    showPaciente(Paciente),
+    showPacientes(T).
 
 verMedicos(IdClinica) :-
     forall(model:medico(IdClinica, IdMed, Nome, CRM, Especialidade, Telefone, Endereco, Senha), 
